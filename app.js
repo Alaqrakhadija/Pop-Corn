58fd09fdca61a23460414bc7c944b0a7d484d44f
laith alaqr <thebosslaith@gmail.com>
2022-04-06 03:25:18 +0300

    fixed routing

--- app.js
+++ app.js
@@ -1,157 +1,136 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
-const query = require('./config/database');
-const { YEAR } = require('mysql/lib/protocol/constants/types');
-
-
-
+const query = require("./config/database");
+const { YEAR } = require("mysql/lib/protocol/constants/types");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.get("/", async (req, res) => {
+  const qyery1 =
+    "SELECT Poster_Link,Series_Title,Released_Year,Genre,IMDB_Rating,Runtime FROM `imdb_top_1000` LIMIT 4";
+  const qyery2 =
+    "SELECT Poster_Link,Series_Title,Overview FROM `imdb_top_1000` LIMIT 6";
 
+  // pool.getConnection((err, connection) => {
 
-app.get('/', async(req, res) => {
+  //     if (err) return;
 
-    const qyery1 = "SELECT Poster_Link,Series_Title,Released_Year,Genre,IMDB_Rating,Runtime FROM `imdb_top_1000` LIMIT 4";
-    const qyery2 = "SELECT Poster_Link,Series_Title,Overview FROM `imdb_top_1000` LIMIT 6";
+  //     connection.query(qyery, (err, result) => {
+  //         console.log(result);
+  //         result1 = result
 
-    // pool.getConnection((err, connection) => {
+  //     })
 
+  // })
 
-    //     if (err) return;
+  const result = await query(qyery1);
+  const result2 = await query(qyery2);
 
-    //     connection.query(qyery, (err, result) => {
-    //         console.log(result);
-    //         result1 = result
-
-    //     })
-
-    // })
-
-    const result = await query(qyery1);
-    const result2 = await query(qyery2);
-
-    console.log(result)
-    res.render("index", { result, result2 });
-
-
-
-})
-
-app.get('/movies/', async(req, res) => {
-
-    const qyery1 = "SELECT Poster_Link,Series_Title,Released_Year,Genre,IMDB_Rating FROM `imdb_top_1000` LIMIT 18";
-
-
-
-
-    const result = await query(qyery1);
-
-
-    console.log(result)
-    res.render("movies", { result });
-
-
-
-})
-app.get('/movies/:catg', async(req, res) => {
-
-    const genre = req.params.catg;
-    const qyery1 = 'SELECT * FROM imdb_top_1000  WHERE Genre LIKE "%' + `${genre}` + '%"';
-
-
-
-
-    const result = await query(qyery1);
-
-
-    console.log(result)
-    res.render("movies", { result });
-
-
-
-})
-
-
-
-
-app.get('/filter', async(req, res) => {
-            let sqlQueryString;
-            const genre = req.query.Genre;
-            const year = req.query.Released_Year ? req.query.Released_Year.split("-") : '';
-            const filters = {
-                    genre: `${genre? genre==="all genres"?"":'Genre LIKE "%'+`${genre}`+'%"':""}`,
-                    year:`${year?year[0]==="allyears"?"":'Released_Year BETWEEN '+`${year[0]}`+' and '+`${year[1]}`:""}`
-}
-console.log(filters.genre+"   "+filters.year);
- if(filters.genre && filters.year){
-    sqlQueryString = 'SELECT Poster_Link,Series_Title,Released_Year,Genre,IMDB_Rating from `imdb_top_1000` where '+`${filters.genre}`+' and '+`${filters.year}`;
-}
-else if(filters.genre.length==0 && filters.year.length==0){
-    sqlQueryString = 'SELECT Poster_Link,Series_Title,Released_Year,Genre,IMDB_Rating from `imdb_top_1000`';
-   
-}
-else{
-    
-    sqlQueryString = 'SELECT Poster_Link,Series_Title,Released_Year,Genre,IMDB_Rating from `imdb_top_1000` where '+`${filters.genre}`+`${filters.year}`;
-
-}
-
-sqlQueryString +=' LIMIT 18';
-  
-   const result = await query(sqlQueryString);
-  res.json({ msg: result });
-    console.log(sqlQueryString);
-    console.log(result);
-
+  console.log(result);
+  res.render("index", { result, result2 });
 });
 
+app.get("/movies", async (req, res) => {
+  const genre = req.query.genre;
+  if (genre) {
+    const qyery1 =
+      'SELECT * FROM imdb_top_1000  WHERE Genre LIKE "%' + `${genre}` + '%"';
 
-
-
-app.get('/category/:catg', async(req, res) => {
-
-    const qyery1 = "SELECT Poster_Link,Series_Title,Released_Year,Genre,IMDB_Rating FROM `imdb_top_1000` where"+`${req.params.catg}`+" LIMIT 18";
     const result = await query(qyery1);
 
-    console.log(result)
-    res.render("movies", { result});
-})
+    res.render("movies", { result, genre: genre });
+  } else {
+    const qyery1 =
+      "SELECT Poster_Link,Series_Title,Released_Year,Genre,IMDB_Rating FROM `imdb_top_1000` LIMIT 18";
 
+    const result = await query(qyery1);
 
+    res.render("movies", { result, genre: "allgenres" });
+  }
+});
+app.get("/movies/:catg", async (req, res) => {
+  const genre = req.params.catg;
+  const qyery1 =
+    'SELECT * FROM imdb_top_1000  WHERE Genre LIKE "%' + `${genre}` + '%"';
 
+  const result = await query(qyery1);
 
-app.get('/search', async(req, res) => {
+  res.render("movies", { result });
+});
 
-    const search = req.query.q;
+app.get("/filter", async (req, res) => {
+  let sqlQueryString;
+  const genre = req.query.Genre;
+  const year = req.query.Released_Year
+    ? req.query.Released_Year.split("-")
+    : "";
+  const filters = {
+    genre: `${
+      genre
+        ? genre === "all genres"
+          ? ""
+          : 'Genre LIKE "%' + `${genre}` + '%"'
+        : ""
+    }`,
+    year: `${
+      year
+        ? year[0] === "allyears"
+          ? ""
+          : "Released_Year BETWEEN " + `${year[0]}` + " and " + `${year[1]}`
+        : ""
+    }`,
+  };
+  console.log(filters.genre + "   " + filters.year);
+  if (filters.genre && filters.year) {
+    sqlQueryString =
+      "SELECT Poster_Link,Series_Title,Released_Year,Genre,IMDB_Rating from `imdb_top_1000` where " +
+      `${filters.genre}` +
+      " and " +
+      `${filters.year}`;
+  } else if (filters.genre.length == 0 && filters.year.length == 0) {
+    sqlQueryString =
+      "SELECT Poster_Link,Series_Title,Released_Year,Genre,IMDB_Rating from `imdb_top_1000`";
+  } else {
+    sqlQueryString =
+      "SELECT Poster_Link,Series_Title,Released_Year,Genre,IMDB_Rating from `imdb_top_1000` where " +
+      `${filters.genre}` +
+      `${filters.year}`;
+  }
 
-    res.json({ msg: "your search is :" + search });
+  sqlQueryString += " LIMIT 18";
 
+  const result = await query(sqlQueryString);
+  res.json({ msg: result });
+  console.log(sqlQueryString);
+  console.log(result);
+});
 
-})
+app.get("/category/:catg", async (req, res) => {
+  const qyery1 =
+    "SELECT Poster_Link,Series_Title,Released_Year,Genre,IMDB_Rating FROM `imdb_top_1000` where" +
+    `${req.params.catg}` +
+    " LIMIT 18";
+  const result = await query(qyery1);
 
+  console.log(result);
+  res.render("movies", { result });
+});
 
+app.get("/search", async (req, res) => {
+  const search = req.query.q;
 
-
-
-
-
-
-
-
+  res.json({ msg: "your search is :" + search });
+});
 
 app.listen(3000, () => {
-
-    console.log('ruunig on 3000');
-
-
+  console.log("ruunig on 3000");
 });
